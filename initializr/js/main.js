e2a8a797f93c5f2c39cbe708d44dd0b7eaea9ea6
Adam Wilson <adamwilson1234@gmail.com>
2016-09-09 15:56:31 -0500

    first commit

---
+++ initializr/js/main.js
@@ -0,0 +1,35 @@
+
+
+/* ====================================
+			GLOBAL VARIABLES
+==================================== */
+var reactData;
+var angularData;
+// define new array. array needed if we will use ng-repeat to enable filtering and sorting
+var allData = [];
+
+
+
+/* ====================================
+
+==================================== */
+
+/* ====================================
+
+==================================== */
+
+/* ====================================
+
+==================================== */
+
+// init function containing
+// initial calls to ajax functions
+// call to repeat function
+
+// update frequency variable definition
+
+
+
+
+// show frequency maybe function
+// show last update function
