86ad0dc5b88a22de2df88638e1f964a305a881e2
Adam Wilson <adamwilson1234@gmail.com>
2016-09-09 22:35:39 -0500

    main data feeds now all make it to page. rendering popularity to dom. refactored ajax function into one function called with url argument.

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -3,11 +3,21 @@
 /* ====================================
     GLOBAL VARIABLES
 ==================================== */
+
+// DATA VARIABLES
 var reactData;
 var angularData;
 var allData = []; // define new array. array needed if we will use ng-repeat to enable filtering and sorting
+
+// UPDATE VARIABLES
 var FREQ = 7560000; // update frequency: just over 2 hours as only 6 calls/hour without Oauth.
 var repeat = true; // turn updates on or off
+
+// API URLS
+var emberURL = 'https://api.github.com/repos/emberjs/ember.js';
+var vueURL = 'https://api.github.com/repos/vuejs/vue';
+var angularURL = 'https://api.github.com/repos/angular/angular.js';
+var reactURL = 'https://api.github.com/repos/facebook/react';
 
 
 
@@ -18,14 +28,32 @@ var repeat = true; // turn updates on or off
     call function to make delayed calls and repeate every 2.1 hours
 ==================================== */
 $(document).ready(function() {
-    getReactData();
-	getAngularData();
-
-
-
+    getData(angularURL);
+    getData(reactURL);
+    getData(emberURL);
+    getData(vueURL);
 
 
 });
+
+
+
+/* ====================================
+    GET DATA OBJECT FOR REPO PASSED AS API URL ARGUMENT
+    and call helper function which combines data with
+    data from other repos
+==================================== */
+function getData(apiURL){
+    $.ajax({
+        url: apiURL,
+        cache: false,
+        dataType: 'JSON',
+        success: function(data){
+            console.log(data);
+            combineData(data);
+        }
+    });
+}
 
 
 
@@ -49,52 +77,9 @@ function makeDelayedCallsAndRepeat(){
 }
 
 
-
-
 /* ====================================
-    GET REACT DATA OBJECT &
-    PUSH IT ONTO MAIN DATA ARRAY
-==================================== */
-function getReactData(){
-    $.ajax({
-        url: 'https://api.github.com/repos/facebook/react',
-        cache: false,
-        dataType: 'JSON',
-        success: function(data){
-            reactData = data;
-            console.log('here is reactData:');
-            console.log(reactData);
-
-            combineData(reactData);
-        }
-    });
-}
-
-
-
-/* ====================================
-    GET REACT DATA OBJECT &
-    PUSH IT ONTO MAIN DATA ARRAY
-==================================== */
-function getAngularData(){
-    $.ajax({
-        url: 'https://api.github.com/repos/angular/angular.js',
-        cache: false,
-        dataType: 'JSON',
-        success: function(data){
-            angularData = data;
-            console.log('here is angularData:');
-            console.log(angularData);
-
-            combineData(angularData);
-        }
-    });
-}
-
-/* ====================================
-    COMBINE DATA FROM AJAX FUNCTIONS
-    they call this helper function once they have their data
-    this saves it onto main data array
+    COMBINE DATA INTO SINGLE ARRAY
+    this saves request data onto main data array
     conditionals used: if react save at index 1, etc.
 ==================================== */
 function combineData(data){
@@ -102,13 +87,13 @@ function combineData(data){
         allData[0] = data;
     } else if ( data.name == 'react'){
         allData[1] = data;
+    } else if ( data.name == 'ember.js'){
+        allData[2] = data;
+    } else if ( data.name == 'vue'){
+        allData[3] = data;
     }
-
-
-    // allData.push(reactData);
     console.log('here is allData:');
     console.log(allData);
-
     renderDataToPage(allData);
 }
 
@@ -119,14 +104,23 @@ function combineData(data){
     after building the allData array with ajax call functions
     let's put the data into the DOM
 ==================================== */
-
 function renderDataToPage(allData){
     console.log('here is allData within renderDataToPage');
     console.log(allData);
     console.log(allData[0].stargazers_count);
 
-    $('#angular-stargazers').empty();
-    $('#angular-stargazers').append(allData[0].stargazers_count);
+    // empty dynamically updated cells so if they don't update with new data we'll notice a problem
+    $('td:first-child').empty();
+    $('td:nth-child(2)').empty();
+    $('td:nth-child(3)').empty();
+
+    // update stargazers
+    $('#angular-stargazers').text(allData[0].stargazers_count);
+    $('#react-stargazers').text(allData[1].stargazers_count);
+    $('#ember-stargazers').text(allData[2].stargazers_count);
+    $('#vue-stargazers').text(allData[3].stargazers_count);
+
+    
 
 
 
@@ -146,3 +140,49 @@ function renderDataToPage(allData){
 
 // show frequency maybe function
 // show last update function
+
+
+
+
+
+
+/* ====================================
+
+    REMOVED OR REFACTORED CODE DISCARDS
+
+==================================== */
+// function getReactData(){
+//     $.ajax({
+//         url: 'https://api.github.com/repos/facebook/react',
+//         cache: false,
+//         dataType: 'JSON',
+//         success: function(data){
+//             reactData = data;
+//             console.log('here is reactData:');
+//             console.log(reactData);
+//
+//             combineData(reactData);
+//         }
+//     });
+// }
+
+
+
+/* ====================================
+    GET REACT DATA OBJECT &
+    PUSH IT ONTO MAIN DATA ARRAY
+==================================== */
+// function getAngularData(){
+//     $.ajax({
+//         url: 'https://api.github.com/repos/angular/angular.js',
+//         cache: false,
+//         dataType: 'JSON',
+//         success: function(data){
+//             angularData = data;
+//             console.log('here is angularData:');
+//             console.log(angularData);
+//
+//             combineData(angularData);
+//         }
+//     });
+// }
